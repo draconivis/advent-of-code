f59bf9dc14b8a0c7f0486a4cfdf7e390e8fabcef
draconivis <patrick.gasser@proton.me>
2024-12-02 11:33:20 +0100

    refactor day1

--- 2024/day1/day1_1.php
+++ 2024/day1/day1_1.php
@@ -12,21 +12,12 @@ while (!feof($file)) {
 	$col2[] = $matches[2];
 }
 
-$sum1 = 0;
-
-// part2
-foreach ($col1 as $value) {
-	$sum1 += $value * count(array_filter($col2, fn($value2) => $value === $value2));
-}
-var_dump('sum2: '.$sum1);
-
-// part1
 sort($col1);
 sort($col2);
-$sum2 = 0;
+$sum = 0;
 foreach ($col1 as $key => $value) {
-	$sum2 += abs($value - $col2[$key]);
+	$sum += abs($value - $col2[$key]);
 }
 
-var_dump('sum1: '.$sum2);
+var_dump($sum);
 
